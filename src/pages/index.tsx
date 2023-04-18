cf2c90f87bf2767a01818bffcd0ea009d5646381
Luis Amengual <luismanuelamengual@gmail.com>
2023-04-18 18:29:28 -0300

    Vuelta a estructura vieja de pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+export default function Page() {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session?.user ? (
+        <>
+          {session.user.image && (
+            <span
+              style={{ backgroundImage: `url('${session.user.image}')` }}
+            />
+          )}
+          <span>
+            <small>Signed in as</small>
+            <br />
+            <strong>{session.user.name ?? session.user.email}</strong>
+          </span>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+
+      ) : (
+        <>
+          <span>
+            You are not signed in
+          </span>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+    </div>
+  );
+}
