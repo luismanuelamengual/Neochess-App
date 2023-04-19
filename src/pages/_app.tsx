c083320e40cd9bee68fcfcddbd5cd8860a9fb398
Luis Amengual <luismanuelamengual@gmail.com>
2023-04-19 08:57:03 -0300

    Add auth actions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { logOut } from '@actions';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Avatar } from '@mui/material';
+import { Avatar, Box, Menu, MenuItem, Tooltip } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { SessionProvider, useSession } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import './styles.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -17,6 +19,21 @@ export default function App({ Component, pageProps }: AppProps) {
 
 export function NavigationAppBar() {
   const { data: session } = useSession();
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogoutClick = () => {
+    handleCloseUserMenu();
+    logOut();
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -32,14 +49,35 @@ export function NavigationAppBar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontFamily: 'fantasy' }}>
             NEOCHESS
         </Typography>
-        <div>
-          <IconButton
-            size="large"
-            color="inherit"
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton
+              size="large"
+              color="inherit"
+              onClick={handleOpenUserMenu}
+            >
+              <Avatar alt={session?.user?.name ?? 'NN'} src={session?.user?.image ?? ''} />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
           >
-            <Avatar alt={session?.user?.name ?? 'NN'} src={session?.user?.image ?? ''} />
-          </IconButton>
-        </div>
+            <MenuItem onClick={handleLogoutClick}><Typography textAlign="center">Logout</Typography></MenuItem>
+          </Menu>
+        </Box>
       </Toolbar>
     </AppBar>
   );
